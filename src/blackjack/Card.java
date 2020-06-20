1159b4f291199c1598315714ce0e08fc6f2686e5
Kennith Yu <yukenn@sheridancollege.ca>
2020-06-20 15:21:40 -0400

    UML Diagram update, Card class updated, BlackjackGame class added

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -6,7 +6,7 @@ package blackjack;
  */
 //this is a test
 public class Card {
-   public enum Suit {
+        public enum Suit {
             HEARTS, DIAMONDS, CLUBS, SPADES
         }
         
---
+++ src/blackjack/Dealer.java
@@ -0,0 +1,31 @@
+package blackjack;
+
+/**
+ *
+ * @authors Shannon Han, Kennith King Hay Yu
+ */
+public class Dealer {
+
+    private Card[] deck = new Card[52];
+
+    public void shuffleDeck() {
+        // TODO - implement Dealer.shuffle
+        throw new UnsupportedOperationException();
+    }
+
+    public void dealHand() {
+        // TODO - implement Dealer.startGame
+        throw new UnsupportedOperationException();
+    }
+
+    public Dealer() {
+        // TODO - implement Dealer.Dealer
+        throw new UnsupportedOperationException();
+    }
+
+    public void createDeck() {
+        // TODO - implement Dealer.createDeck
+        throw new UnsupportedOperationException();
+    }
+    
+}
